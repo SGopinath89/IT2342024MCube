01b16a900e8d49e8158662801ed7115f6c57592f
Musny Mubarak <msnymohammed@gmail.com>
2024-05-17 09:56:07 +0530

    add backend

---
+++ backend/index.js
@@ -0,0 +1,87 @@
+const port = 4000;
+const express = require("express");
+const app = express();
+const mongoose = require('mongoose');
+const multer = require("multer");
+const path = require("path");
+const cors = require("cors");
+
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect("mongodb+srv://mcube:mcube123@cluster0.ojhuygi.mongodb.net/mcube?retryWrites=true&w=majority&appName=Cluster0");
+
+// Serve static files
+app.use('/images', express.static('upload/images'));
+
+// Image storage
+const storage = multer.diskStorage({
+    destination: './upload/images',
+    filename: (req, file, cb) => {
+        return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`);
+    }
+});
+
+const upload = multer({ storage: storage });
+
+const Product = mongoose.model("Product", {
+    id: {
+        type: Number,
+        required: true,
+    },
+    name: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String, // Assuming image URL will be stored
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    new_price: {
+        type: Number,
+        required: true,
+    },
+    old_price: {
+        type: Number,
+        required: true,
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+    available: {
+        type: Boolean, // Corrected typo
+        required: true,
+    }
+});
+
+app.post('/addproduct', async (req, res) => {
+    const product = new Product({
+        id: req.body.id,
+        name: req.body.name,
+        image: req.body.image,
+        category: req.body.category,
+        new_price: req.body.new_price,
+        old_price: req.body.old_price,
+        available: req.body.available
+    });
+
+    try {
+        const savedProduct = await product.save();
+        res.json(savedProduct);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+});
+
+app.listen(port, (error) => {
+    if (!error) {
+        console.log("Server is running on port " + port);
+    } else {
+        console.log("Error: " + error);
+    }
+});
