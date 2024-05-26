7930bc60e772363abf24c1016b79be24f6b60151
musnymubarak <musnymohammed@gmail.com>
2024-05-26 12:01:57 +0530

    add,remove products

--- backend/index.js
+++ backend/index.js
@@ -63,18 +63,24 @@ const Product = mongoose.model("Product", {
         default: Date.now,
     },
     available: {
-        type: Boolean, // Corrected typo
-        required: true,
+        type: Boolean,
+        default: true,
     }
 });
 
-app.get("/",(req,res)=>{
-    res.send("Express app is running")
-})
-
 app.post('/addproduct', async (req, res) => {
+    let products = await Product.find({});
+    let id;
+    if(products.length>0){
+        let last_product_array = products.slice(-1);
+        let last_product = last_product_array[0];
+        id = last_product.id+1;
+    }
+    else {
+        id=1;
+    }
     const product = new Product({
-        id: req.body.id,
+        id: id,
         name: req.body.name,
         image: req.body.image,
         category: req.body.category,
@@ -83,14 +89,32 @@ app.post('/addproduct', async (req, res) => {
         available: req.body.available
     });
 
-    try {
-        const savedProduct = await product.save();
-        res.json(savedProduct);
-    } catch (error) {
-        res.status(400).json({ message: error.message });
-    }
+   console.log(product);
+   await product.save();
+   console.log("Saved");
+   res.json({
+    success:true,
+    name:req.body.name,
+   })
 });
 
+
+//Delete Products
+app.post('/removeproduct',async (req,res)=>{
+    await Product.findOneAndDelete({id:req.body.id});
+    console.log("Removed");
+    res.json({
+        success:true,
+        name:req.body.name
+    })
+})
+
+//Get ALL products
+app.get('/allproducts',async (req,res)=>{
+    let products = await Product.find({});
+    console.log("All products fetched");
+    res.send(products);
+})
 
 app.listen(port, (error) => {
     if (!error) {
@@ -99,3 +123,9 @@ app.listen(port, (error) => {
         console.log("Error: " + error);
     }
 });
+
+
+app.get("/",(req,res)=>{
+    res.send("Express app is running")
+})
+
