8ace658dd0e2ceec5b7a7179df9a18487d0143e2
musnymubarak <musnymohammed@gmail.com>
2024-05-26 12:50:33 +0530

    User registration

--- backend/index.js
+++ backend/index.js
@@ -116,6 +116,56 @@ app.get('/allproducts',async (req,res)=>{
     res.send(products);
 })
 
+//User Model
+const Users = mongoose.model('Users',{
+    name:{
+        type:String
+    },
+    email:{
+        type:String
+    },
+    password:{
+        type:String
+    },
+    cartData:{
+        type:Object
+    },
+    date:{
+        type:Date,
+        default:Date.now
+    }
+})
+
+//Register the user
+app.post('/signup',async (req,res)=>{
+    let check = await Users.findOne({email:req.body.email});
+    if(check){
+        return res.status(400).json({success:false,errors:"existing user found"});
+    }
+    let cart = {};
+    for(let i=0; i<300; i++){
+        cart[i]=0;
+    }
+    const user = new Users({
+        name:req.body.username,
+        email:req.body.email,
+        password:req.body.password,
+        cartData:cart
+    })
+
+    await user.save();
+
+    const data = {
+        user:{
+            id:user.id
+        }
+    }
+
+    const token = jwt.sign(data, 'secret_mcube');
+    res.json({success:true,token})
+
+})
+
 app.listen(port, (error) => {
     if (!error) {
         console.log("Server is running on port " + port);
